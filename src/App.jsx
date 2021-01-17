efeeaaf7915831a7ecf502cfe0e5fc2ba08a49e6
senpai-10 <bmjfdrh@gmail.com>
2021-01-18 00:39:35 +0300

    import NewPassword component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 
 // import Passwords from './components/Passwords'
+import NewPassword from './components/NewPassword'
 
 function App() {
   return (
     <div>
-
+      <NewPassword />
     </div>
   )
 }
