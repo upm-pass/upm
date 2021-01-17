3935558c97236231eccdbcea14307358b08b1760
senpai-10 <bmjfdrh@gmail.com>
2021-01-17 21:55:39 +0300

    create passwords component

---
+++ src/components/Passwords.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Passwords extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hello</h1>
+            </div>
+        )
+    }
+}
+
+export default Passwords
