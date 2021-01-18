fb51527c599e72d6b9f5ef04389102507e5e4b52
senpai-10 <bmjfdrh@gmail.com>
2021-01-18 04:40:34 +0300

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import storage from 'electron-localstorage'
 
 // import Passwords from './components/Passwords'
 import NewPassword from './components/NewPassword'
+
+
 
 function App() {
   return (
     <div>
       <NewPassword />
+      {console.log(storage.clear())}
     </div>
   )
 }
