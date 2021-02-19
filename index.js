af362fe29a5780016f5dff8a6653eca2d2ca9624
Senpai-10 <bmjfdrh@gmail.com>
2021-02-19 22:21:25 +0300

    config command

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 const clicolor = require("cli-color")
 const editjsonfile = require("edit-json-file")
 const input = require("input")
-let config = editjsonfile("/home/senpai/epm.json")
+let config = editjsonfile("/home/senpai/epm.json", {autosave: true})
 
 const [,, ...args] = process.argv
 
 const commands = [
-    'masterkey',
+    'config',
     'ls',
     'add',
     'remove',
@@ -16,7 +16,7 @@ const commands = [
 ]
 
 const options = {
-    "masterkey": [""],
+    "config": [""],
     "ls": [""],
     "add": [""],
     "remove": ["-a", "--all"],
@@ -24,8 +24,47 @@ const options = {
 }
 
 const app = async () => {
-    if (args[0] == commands[0]) { // set masterkey
-        console.log("set master key command")
+    if (!config.get("MasterKey")) {
+        console.log(`
+Welcome, to epm setup 
+ You need to give me a master key
+  To see, edit, remove passwords.
+
+        `)
+
+        Masterkey  = await input.password("Master key: ")
+        ReMasterkey = await input.password("Retype - Master key: ")
+
+        if (Masterkey != ReMasterkey) {
+            console.log(`\nRetype - Master key does not match Master key password`)
+        } else {
+            config.set("MasterKey", Masterkey)
+            console.log("\nPassword has been successfully saved.");
+        }
+
+        process.exit()
+    }
+
+    if (args[0] == commands[0]) { // set config
+        console.log(`
+1. change password
+        `)
+
+        choice = await input.text("choice: "); console.log('\n')
+        
+        if (choice == 1) {
+            OldPass = await input.text("old master password: ")
+
+            if (OldPass === config.get("MasterKey")) {
+                NewPass = await input.text("new master password: ")
+                
+                config.set("MasterKey", NewPass)
+                console.log("\nPassword has been successfully saved.")
+            
+            } else {
+                console.log("\nold password is wrong\n")
+            }
+        }
     }
 
     if (args[0] == commands[1]) { // ls list all passwords
@@ -33,8 +72,9 @@ const app = async () => {
     }
 
     if (args[0] == commands[2] && args[1]) { // add
-        password = await input.password(`password for ${args[1]}: `)
-        
+        DomainName = args[1]
+        password = await input.password(`password for ${DomainName}: `)
+        config.set("passwords." + DomainName, password)
     }
 
     if (args[0] == commands[3]) { // remove
